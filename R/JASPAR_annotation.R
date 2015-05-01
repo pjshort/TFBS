8a8c48d5ade7def9918438095fe4fdd1bae370a1
Patrick Short <ps14@sanger.ac.uk>
2015-05-01 11:26:59 +0100

    split JASPAR_annotation.R into generate_TF_list (for pre-processing) and JASPAR_annotation.R. JASPAR annotation can now be much more easily run in an array

--- R/JASPAR_annotation.R
+++ R/JASPAR_annotation.R
@@ -1,9 +1,7 @@
 # scan all of the well-covered regions with the pwm list and save output
 
 # dependencies and libraries
-source("simulate.R")
 source("tfbs_core.R") # depends on TFBSTools, JASPAR2014, BioStrings
-source("visualization.R")
 library(optparse)
 
 # command line options
@@ -12,12 +10,14 @@ option_list <- list(
               help="Print extra output advising the user of progression through the code."),
   make_option("--regions", default="../data/regions_annotated.txt",
               help="Pass the genomic regions that should be annotated with predicted TF binding sites."),
-  make_option("--de_novos", default="../data/de_novo_filtered.txt",
-              help="Pass the observed de novos - these will be analyzed in order to shorten the list of TF binding sites used for prediction."),
-  make_option("--full_jaspar", action="store_true", default=TRUE,
-              help="If TRUE annotates regions with the full JASPAR Homo Sapiens set."),
+  make_option("--tf_list", default="../data/TFs_in_DDD_data.txt",
+              help="Pass a list of TFs to be run against regions."),
+  make_option("--full_jaspar", action="store_true", default=FALSE,
+              help="If TRUE annotates regions with the full JASPAR Homo Sapiens set. If false, must provide a --tflist"),
   make_option("--out", default="../data/regions_JASPAR_annotated.txt",
-              help="Set location to save the (likely large) tab-delimited text file storing JASPAR annotations.")
+              help="Set location to save the (likely large) tab-delimited text file storing JASPAR annotations."),
+  make_option("--min_score", default="95%",
+              help="Set min score for the JASPAR annotations - less than 95% may yield unrealistic number of predicted binding sites.")
 )
 
 args <- parse_args(OptionParser(option_list=option_list))
@@ -30,47 +30,26 @@ if ( args$verbose ) {
 # NOTE: only need to initalize DB once - this should be done on install...?
 #db = "myMatrixDb.sqlite"
 #initializeJASPARDB(db)
-opts = list("species" = 9606, "all_versions" = args$full_jaspar, "matrixtype" = "PWM") # 9606 = "homo sapiens"
+opts = list("species" = 9606, "all_versions" = TRUE, "matrixtype" = "PWM") # 9606 = "homo sapiens"
 pwm_list = getMatrixSet(JASPAR2014, opts)
 
-
-# de novos are filtered for pp_dnm > 0.1 and non-coding only
-de_novo_filtered <- read.table(args$de_novos, sep="\t", header=TRUE)
+# load the names of TFs that we want to scan for
+TFBS_to_scan = read.table("../data/TFs_in_DDD_data.txt", header = TRUE)
 
 # regions are annotated by n_snps and n_indels - created by rupit/R/pre_process.R
 well_covered_regions <- read.table(as.character(args$regions), sep="\t", header=TRUE)
 
-if ( args$verbose ) {
-  write("Processing regions input and de novos for primary scan.", stderr())
+# scan only on the TFBS specified in input file
+if (args$full_jaspar == FALSE){  # if args$full_jaspar is TRUE use the entire JASPAR list - will be huge for large set of regions
+  pwm_list_reduced = pwm_list[unique(TFBS_to_scan$jaspar_internal)]
 }
-# make new 'de novo containing regions' (dcr) data frame
-dcr = well_covered_regions[well_covered_regions$n_snp > 0 | well_covered_regions$n_indel > 0, ]
-dcr = merge(dcr, de_novo_filtered[, c("region_id", "pos", "ref", "alt", "person_stable_id")], by = "region_id")
-dcr$rel_pos = dcr$pos - dcr$start
-
-if ( args$verbose ) {
-  write("Starting primary scan to identify TFBS of interest from de novo set.", stderr())
-}
-# get matrix (TF x dcr) with 1 if de novo in that region disrupts TF binding
-ninety_five_counts = mapply(TFBS_hits, dcr$seq, dcr$rel_pos, MoreArgs = list("pwm_list" = pwm_list, "min.score" = "95%"))
-
-# get number of TFBS disruptions per de novo
-dcr$TFBS_count = colSums(ninety_five_counts)
-
-# get number of disruptions per TFBS
-counts_by_TFBS = rowSums(ninety_five_counts)
-
-# get only the TFBS that have some annotation
-recurrent_TFBS = names(counts_by_TFBS)[counts_by_TFBS > 0]
-pwm_list = pwm_list[recurrent_TFBS]
 
 # NOTE: this scanning is the longest step to complete
 if ( args$verbose ) {
   write("Scanning all regions supplied against TFBS identified in primary scan.", stderr())
 }
 
-# EDIT THIS - ONLY FOR TESTING
-scanned_regions = scan_regions(well_covered_regions[1:68,], pwm_list, min.score = "95%")
+scanned_regions = scan_regions(well_covered_regions[1:10,], pwm_list_reduced, min.score = args$min_score)
 
 # make a dataframe containing region IDs and predicted TF binding sites in these regions
 region_dfs = mapply(function(s, n) data.frame(region_id = rep(n, length(s)), start=s@start, stop=s@start+s@width - 1, name = s@NAMES), scanned_regions, names(scanned_regions), SIMPLIFY = FALSE)
@@ -84,12 +63,6 @@ write.table(full_df, file = args$out, row.names = FALSE, sep = "\t", col.names =
 if ( args$verbose ) {
   write(sprintf("Finished! Regions annotated by JASPAR and saved to: %s", args$out), stderr())
 }
-
-
-
-# PICK UP HERE TOMORROW - want to run this job in an array with different chunks of well_covered_regions
-# ultimately these will be combined into the regions_JASPAR_annotated.txt that we require
-
 
 
 
---
+++ R/generate_TF_list.R
@@ -0,0 +1,64 @@
+# scan DDD data against all JASPAR TFs
+
+source("tfbs_core.R") # depends on TFBSTools, JASPAR2014, BioStrings
+library(optparse)
+
+# command line options
+option_list <- list(
+  make_option("--verbose", action="store_true", default=TRUE,
+              help="Print extra output advising the user of progression through the code."),
+  make_option("--regions", default="../data/regions_annotated.txt",
+              help="Pass the genomic regions that should be annotated with predicted TF binding sites."),
+  make_option("--de_novos", default="../data/de_novo_filtered.txt",
+              help="Pass the observed de novos - these will be analyzed in order to shorten the list of TF binding sites used for prediction."),
+  make_option("--out", default="../data/TFs_in_DDD_data.txt",
+              help="Set location to save the (likely large) tab-delimited text file storing JASPAR annotations.")
+)
+
+args <- parse_args(OptionParser(option_list=option_list))
+
+# load full PWM list
+if ( args$verbose ) {
+  write("Loading JASPAR position weight matrices from database...", stderr())
+}
+
+# NOTE: only need to initalize DB once - this should be done on install...?
+#db = "myMatrixDb.sqlite"
+#initializeJASPARDB(db)
+opts = list("species" = 9606, "all_versions" = TRUE, "matrixtype" = "PWM") # 9606 = "homo sapiens"
+pwm_list = getMatrixSet(JASPAR2014, opts)
+
+# de novos are filtered for pp_dnm > 0.1 and non-coding only
+de_novo_filtered <- read.table(args$de_novos, sep="\t", header=TRUE)
+
+# regions are annotated by n_snps and n_indels - created by rupit/R/pre_process.R
+well_covered_regions <- read.table(as.character(args$regions), sep="\t", header=TRUE)
+
+if ( args$verbose ) {
+  write("Processing regions input and de novos for primary scan.", stderr())
+}
+# make new 'de novo containing regions' (dcr) data frame
+dcr = well_covered_regions[well_covered_regions$n_snp > 0 | well_covered_regions$n_indel > 0, ]
+dcr = merge(dcr, de_novo_filtered[, c("region_id", "pos", "ref", "alt", "person_stable_id")], by = "region_id")
+dcr$rel_pos = dcr$pos - dcr$start
+
+dcr = dcr[1:20,]
+
+if ( args$verbose ) {
+  write("Starting primary scan to identify TFBS of interest from de novo set.", stderr())
+}
+# get matrix (TF x dcr) with 1 if de novo in that region disrupts TF binding
+ninety_five_counts = mapply(TFBS_hits, dcr$seq, dcr$rel_pos, MoreArgs = list("pwm_list" = pwm_list, "min.score" = "95%"))
+
+# get number of TFBS disruptions per de novo
+dcr$TFBS_count = colSums(ninety_five_counts)
+
+# get number of disruptions per TFBS
+counts_by_TFBS = rowSums(ninety_five_counts)
+
+# get only the TFBS that have some annotation
+recurrent_TFBS = names(counts_by_TFBS)[counts_by_TFBS > 0]
+
+# save one column file with JASPAR internal names of TFs
+write.table(unique(recurrent_TFBS), file = args$out, row.names = FALSE, col.names = "jaspar_internal")
+
