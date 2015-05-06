96b7a380da302981a31019f699d9166cbcae7d4d
Patrick Short <ps14@sanger.ac.uk>
2015-05-06 10:23:01 +0100

    changed default for simulate_and_compare to full simulation history. setting --mean_only=true just gives summary stats.

--- analysis/simulate_and_compare.R
+++ analysis/simulate_and_compare.R
@@ -10,16 +10,17 @@ library(optparse)
 option_list <- list(
   make_option("--n_snps", default=NULL,
               help="Pass the genomic regions that should be annotated with predicted TF binding sites."),
-  make_option("--iterations", default=3,
+  make_option("--iterations", default=20,
               help="Pass a list of TFs to be run against regions."),
-  make_option("--out", default="../data/TFBS_simulation_stats.txt",
+  make_option("--mean_only", action="store_true", default=FALSE),  # flag this option for much smaller output fileb
+  make_option("--out", default="../data/TFBS_simulation_annotation.txt",
               help="Set location to save the (likely large) tab-delimited text file storing JASPAR annotations."),
   make_option("--jaspar_annotated_regions", default="../data/regions_JASPAR_annotated_FULL.txt",
               help="Pass a list of TFs to be run against regions."),
   make_option("--min_score", default="95%",
               help="Set min score for the JASPAR annotations - less than 95% may yield unrealistic number of predicted binding sites."),
   make_option("--verbose", action="store_true", default=TRUE,
-              help="Print extra output advising the user of progression through the code.")
+              help="Print extra output advising the user of progression through the analysis.")
 )
 
 args <- parse_args(OptionParser(option_list=option_list))
@@ -65,6 +66,7 @@ sim_output = simulate_de_novos(well_covered_regions, seq_probabilities, snp_tota
 if ( args$verbose ) {
   write("Annotating simulated de novos with predicted TFBS...", stderr())
 }
+
 annotations = jaspar_annotate(sim_output, JASPAR_annotation) 
 sim_output = annotations[[1]] # adds another layer to sim_output with number of TFBS disruptions
 sim_TFBS_tables = annotations[[2]] # list of tables with TFBS hits per simulation
@@ -74,18 +76,32 @@ sim_TFBS_tables = annotations[[2]] # list of tables with TFBS hits per simulatio
 if ( args$verbose ) {
   write("Calculating summary statistics (mean, SD, SE)...", stderr())
 }
+
 # get mean, SD, and SE on number of hits for each TF
 empty_table = table(jaspar_internal) - 1 # used to standardize names on table
 standardized = do.call(rbind, lapply(sim_TFBS_tables, function(t2) table_diff(t2, empty_table)))
-sim_mean = apply(standardized, MARGIN=2, mean)
-sim_sd = apply(standardized, MARGIN=2, sd)
-sim_se = sim_sd/sqrt(iterations)
 
-# save all of this data as a tab-delimited text file
-TFBS_binding_results = data.frame("jaspar_internal" = jaspar_internal, "tf_names" = tf_names, "sim_mean" = sim_mean, "sim_sd" = sim_sd, "sim_se" = sim_se, row.names = NULL)
-write.table(TFBS_binding_results, file = args$out, row.names = FALSE, sep = "\t", col.names = TRUE)
-if ( args$verbose ) {
-  write(sprintf("Finished! Simulated de novos analyzed by JASPAR. Stats aggregated and saved to: %s", args$out), stderr())
+
+if ( args$mean_only) { # don't record result of every simulation
+  
+  sim_mean = apply(standardized, MARGIN=2, mean)
+  sim_sd = apply(standardized, MARGIN=2, sd)
+  sim_se = sim_sd/sqrt(iterations)
+  
+  # save all of this data as a tab-delimited text file
+  TFBS_binding_results = data.frame("jaspar_internal" = colnames(standardized), "tf_names" = 
+                                      as.character(sapply(colnames(standardized), function(x) pwm_list[x][[1]]@name)), 
+                                    "sim_mean" = sim_mean, "sim_sd" = sim_sd, "sim_se" = sim_se, row.names = NULL)
+  write.table(TFBS_binding_results, file = args$out, row.names = FALSE, sep = "\t", col.names = TRUE)
+  if ( args$verbose ) {
+    write(sprintf("Finished! Simulated de novos analyzed by JASPAR. Stats aggregated and saved to: %s", args$out), stderr())
+  }
+  
+} else { # record result of every simulation
+  standardized = t(standardized)
+  tf_names = as.character(sapply(rownames(standardized), function(x) pwm_list[x][[1]]@name))
+  TFBS_binding_results = cbind(rownames(standardized), tf_names, standardized)
+  write.table(TFBS_binding_results, file = args$out, row.names = FALSE, sep = ",", col.names = FALSE)
 }
 
 
